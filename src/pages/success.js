6bf708211e5ad5746a069e0700db7779f71a99a0
MuhammdSheraz <mshazy999@gmail.com>
2021-05-25 10:44:04 +0500

    success page added

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,9 +1,30 @@
+import { CheckCircleIcon } from "@heroicons/react/solid";
+import Image from "next/image";
 import React from "react";
+import Header from "../components/Header";
 
 function success() {
   return (
-    <div>
-      <h1 className="text-green-300">Thank You For Your Order</h1>
+    <div className="bg-gray-100 h-screen">
+      <Header />
+      <main>
+        <div className="flex flex-col  space-y-3 p-5 px-10 bg-white mx-auto">
+          <div className="flex items-center">
+            <CheckCircleIcon className="text-green-500 h-10" />
+            <h2 className="text-xl md:text-3xl">
+              Thank you,Your Order has been confirmed!
+            </h2>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic dolor
+            dolore deleniti inventore tenetur ea eius ex, quidem aperiam,
+            deserunt, ipsum quaerat soluta praesentium esse?
+          </p>
+          <div className="w-full space-x-7">
+            <button className="addButton w-full ">Go to My Orders</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
